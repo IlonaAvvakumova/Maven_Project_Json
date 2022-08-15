cb8202ea58aae71e7aec4c2c5cf1d8d3392c8434
Ilona <avvakumova_ilona@mail.ru>
2022-08-15 15:05:07 +0300

    test4

--- src/main/java/com/json/gson_test1/view/Main.java
+++ src/main/java/com/json/gson_test1/view/Main.java
@@ -13,32 +13,36 @@ public class Main {
     public static void main(String[] args) {
         LabelRepository lab = new LabelRepository();
         LabelController label = new LabelController();
-        Label la;
+        Label la = null;
         List<Label> listlabel = new ArrayList<>();
         System.out.println("Ввести книгу? да или нет?");
         Scanner scan = new Scanner(System.in);
-       String answer =  scan.nextLine();
+        String answer = scan.nextLine();
 
-       while (answer.equals("да")) {
-           la = new Label();
-           label.labelControll(la); // задать параметры для класса Label
-           lab.save(la); //создать файл json от класса Label
-           listlabel.add(la);
-           System.out.println("Ввести еще книгу? да или нет?");
-          answer =  scan.nextLine();
+        while (answer.equals("да")) {
+            la = new Label();
+            label.labelControll(la); // задать параметры для класса Label
+            lab.save(la); //создать файл json от класса Label
+            listlabel.add(la);
+            System.out.println("Ввести еще книгу? да или нет?");
+            answer = scan.nextLine();
 
-       }
-        System.out.println("Получить список книг:");
-        System.out.println(listlabel);
+        }
 
-        System.out.println("Какой id нужен?");
-         Integer answer2 =  scan.nextInt();
 
-  List<Label> list2 =  listlabel.stream().filter(a-> a.getId()==answer2 ).collect(Collectors.toList());
+       System.out.println("Какой id нужно получить?");
+        Integer answer2 = scan.nextInt();
+        List<Label> list2 = listlabel.stream().filter(a -> a.getId() == answer2).collect(Collectors.toList());
+
         System.out.println("Книга под id: " + answer2 + " - " + list2);
+
+        System.out.println("Какой id нужно удалить?");
+        Integer answer3 = scan.nextInt();
+
+        List<Label> list3 = listlabel.stream().dropWhile(a->a.getId()==answer3).collect(Collectors.toList());
+
 
 
     }
-
 
 }
